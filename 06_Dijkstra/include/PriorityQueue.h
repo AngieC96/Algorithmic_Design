dcc8e1c30f527ef549967b5f3f9ce0da6ce6a47b
Angela Carraro <angela_carraro@hotmail.it>
2020-09-20 12:38:59 +0200

    Finished PriorityQueue

---
+++ 06_Dijkstra/include/PriorityQueue.h
@@ -0,0 +1,128 @@
+#ifndef __PQUEUE__
+#define __PQUEUE__
+
+/**
+ * @file
+ *
+ * It will contain the Priority Queue-related code
+ */
+
+#include <stdlib.h>
+
+#include "List.h"
+#include "utilities.h"
+
+/** @struct ListNode
+ *  @brief A class to represent a node of a list.
+ *
+ *  This class is meant to represent a node of a list.
+ */
+typedef struct QueueNode{
+    /** @brief Value inside the list node: any data type can be stored in it. */
+    void* T;
+    /** @brief Priority of the node: ower values indicate higher priority  (no negative edges so no negative distances = priorities). */
+    size_t priority;
+    /** @brief Pointer to the next element of the list. */
+    struct QueueNode* next;
+} QueueNode;
+
+/** @struct PriorityQueue
+ *  @brief A class to represent a priority queue.
+ *
+ *  This class is meant to represent a priority queue.
+ */
+typedef struct {
+    /** @brief The list used to store the queue elements. */
+    QueueNode* head;
+    /** @brief The type size in bytes of the elements in Q. */
+    size_t elem_size;
+} PriorityQueue;
+
+/**********************************************************************
+ * Test whether the priority queue is empty.
+ *
+ * This function tests whether a priority queue is empty. If it is the 
+ * case, it returns a value different from 0, otherwise, 0 is returned. 
+ * The asymptotic time-complexity of this function is Theta(1).
+ *
+ * @param Q is the queue whose emptyness has to be tested.
+ * @return if Q is not empty, the function returns 0; a number different 
+ *         from 0, otherwise.
+ **********************************************************************/
+int is_queue_empty(const PriorityQueue *Q);
+
+/**********************************************************************
+ * Returns the tail of the priority queue.
+ *
+ * This function returns the tail of the priority queue.
+ *
+ * @param Q is the queue whose tail has to be found.
+ * @return the tail of the queue.
+ **********************************************************************/
+QueueNode* Qtail(PriorityQueue* Q);
+
+/**********************************************************************
+ * Extract the minimum from the heap and return a pointer to it. !!!!!!!!!!!!!!!!!!!!!!!!
+ *
+ * This function extracts the minimum value in the heap w.r.t. its 
+ * total order and returns a pointer to it. If the heap is empty, the 
+ * heap is left unchanged and NULL is returned. The asymptotic 
+ * time-complexity of this function is O(log n).
+ *
+ * @param H is the heap from which the minimum should be extracted.
+ * @return a pointer to the minimum in the heap, if H is not empty; 
+ *         NULL otherwise.
+ **********************************************************************/
+const QueueNode* extract_min(PriorityQueue *Q);
+
+/**********************************************************************
+ * Decrease the value of node's key.  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ *
+ * This function decreases the value of a node's key and returns a 
+ * pointer to the node in which the new value is stored. If either the 
+ * new value is greater than of equal to the old key value or the node 
+ * does not belong to the considered heap, the heap remains unchanged 
+ * and NULL is returned. The asymptotic time-complexity of this 
+ * function is O(log n).
+ *
+ * @param H is the heap in which the key value should be decreased.
+ * @param node is a pointer to the node whose key should be decreased.
+ * @param value is a pointer to the new value of the node's key.
+ * @return a pointer to the node in which the new value is stored. If 
+ *         either the new value is greater than of equal to the old key
+ *         value or the node does not belong to the H, NULL is returned.
+ **********************************************************************/
+const QueueNode* decrease_key(PriorityQueue* Q, QueueNode* node, const size_t value);
+
+/**********************************************************************
+ * Insert a value in the heap.   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ *
+ * This function creates a new node and inserts the passed value as the
+ * key of it. The function returns a pointer to the new node. If the 
+ * heap already contains the maximum number of admissible nodes, then 
+ * the heap remains unchanged and NULL is returned. The asymptotic 
+ * time-complexity of this function is O(log n).
+ *
+ * @param H is the heap in which the key value should be decreased.
+ * @param node is a pointer to the node whose key should be decreased.
+ * @param value is a pointer to the new value of the node's key.
+ * @return a pointer to the node in which the new value is stored. If 
+ *         either the new value is greater than of equal to the old key
+ *         value or the node does not belong to the H, NULL is returned.
+ **********************************************************************/
+const void *insert_value(PriorityQueue* Q, const void* value, size_t priority);
+
+/**********************************************************************
+ * Print a representation of the heap on the standard output.
+ *
+ * This function print a representation of the heap on the standard 
+ * output. Since key type is unknown, a function to print a key value 
+ * is required. The asymptotic time-complexity of this function is 
+ * Theta(n).
+ *
+ * @param H is the heap in which the key value should be decreased.
+ * @param key_printer is a function to print a key value.
+ **********************************************************************/
+void print_pqueue(const PriorityQueue* Q, void (*key_printer)(const void *value));
+
+#endif // __PQUEUE__
---
+++ 06_Dijkstra/src/PriorityQueue.c
@@ -0,0 +1,102 @@
+#include <PriorityQueue.h>
+
+
+int is_queue_empty(const PriorityQueue *Q)
+{
+    return Q->head == NULL;
+}
+
+QueueNode* Qtail(PriorityQueue* Q)
+{
+    QueueNode* tmp = Q->head;
+    while (tmp && tmp->next)
+        tmp = tmp->next;
+
+    return tmp;
+}
+
+const QueueNode* extract_min(PriorityQueue *Q)
+{
+    if(is_queue_empty(Q))
+        return NULL;
+
+    // The minimum is stored in the first element of the queue, since it is ordered
+    // So remove the head and subsitute it with the second element
+    QueueNode* tmp = Q->head;
+    Q->head = tmp->next;
+    
+    return tmp;
+}
+
+const QueueNode* decrease_key(PriorityQueue* Q, QueueNode* node, const size_t value)
+{
+    if(is_queue_empty(Q))
+        return NULL;
+    
+    // We decrease the priority of the node
+    QueueNode *tmp = Q->head, *tmp2, *smaller_node, *prev_node;
+    while (tmp && tmp->next && tmp != node){
+        if (tmp->priority < value){
+            smaller_node = tmp;  // I save the node that should be just before the node with the decreased key in the ordered list
+        }
+        prev_node = tmp;  // I keep track of the node before the one to decrease
+        tmp = tmp->next;
+    }
+
+    // We change the priority of the node
+    tmp->priority = value;
+    // Now we insert the node with the new priority in the right place so the queue is sorted again
+    tmp2 = tmp->next;
+    if(Q->head->priority >= value){
+        tmp->next = Q->head;
+        Q->head = tmp;
+    } else {
+        tmp->next = smaller_node->next;
+        smaller_node->next = tmp;
+    }
+    prev_node->next = tmp2;
+
+    return tmp;
+}
+
+const void *insert_value(PriorityQueue* Q, const void* value, size_t priority)
+{
+    // Allocate memory for the new node
+    QueueNode* newNode = (QueueNode*) malloc(sizeof(QueueNode));
+    newNode->T = malloc(Q->elem_size);
+    newNode->priority = priority;
+    newNode->next = NULL; // not granted that a newly created pointer is NULL
+
+    // Copy contents of new_data to newly allocated memory. 
+    // Assumption: char takes 1 byte. 
+    int i; 
+    for (i=0; i<Q->elem_size; i++)
+        *(char *)(newNode->T + i) = *(char *)(value + i);
+
+    // Change tail pointer as new node is added at the end
+    // We have to check if we have a NULL head (the list is empty), because in this case we have head = tail = newNode.
+    if (Q->head == NULL) {
+        Q->head = newNode;
+    } else {
+        Qtail(Q)->next = newNode;
+    }
+
+    return newNode;
+}
+
+void print_pqueue(const PriorityQueue* Q, void (*key_printer)(const void *value))
+{
+    printf("Content of the queue: ");
+    QueueNode* tmp = Q->head;
+    while (tmp && tmp->next){
+        printf("(");
+        key_printer(tmp->T);
+        printf("%ld), ", tmp->priority);
+        tmp = tmp->next;
+    }
+    printf("(");
+        key_printer(tmp->T);
+        printf("%ld)\n", tmp->priority);
+
+    return;
+}
