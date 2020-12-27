cbeb2b9a7859ce101717b1a3628e0dfe6966df4b
Angela Carraro <angela_carraro@hotmail.it>
2020-12-27 17:59:36 +0100

    File created

---
+++ 06_Dijkstra/include/Dijkstra.h
@@ -0,0 +1,21 @@
+#ifndef __DIJKSTRA__
+#define __DIJKSTRA__
+
+/**
+ * @file
+ *
+ * It will contain the Dijkstra Algorithm.
+ * 
+ * Single Source Shortest Paths: 
+ * We want to compute all the shortest paths from a single node s.
+ */
+
+#include <stdlib.h>
+
+#include "List.h"
+#include "Graph.h"
+
+
+
+
+#endif // __DIJKSTRA__
