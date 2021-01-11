30aec0409d7b479754785226bba7205a3d7d5ed1
Angela Carraro <angela_carraro@hotmail.it>
2021-01-11 22:19:20 +0100

    Still working on dijkstra_binheap() - Need two heapify otherwise not working

--- 06_Dijkstra/src/Dijkstra.c
+++ 06_Dijkstra/src/Dijkstra.c
@@ -35,20 +35,36 @@ void dijkstra_array(Graph* G, int s)
 
 void relax_heap(binheap_type* Q, Node* u, Node* v, int w)
 {
+    printf("\tQ.A before heapify: ");
+    for(int i = 0; i < Q->num_of_elem; ++i)
+        printf("[%d, %d] ", ((Node*)Q->A)[i].key, ((Node*)Q->A)[i].d);
+    printf("\n");
+    //heapify(Q, 0);
+    printf("\tQ.A after heapify: ");
+    for(int i = 0; i < Q->num_of_elem; ++i)
+        printf("[%d, %d] ", ((Node*)Q->A)[i].key, ((Node*)Q->A)[i].d);
+    printf("\n");
     if(u->d + w < v->d){
         // Update distance in the queue Q
         // I need to manually modify the value of d in the node of the queue, since with decrease_key I cannot d it (it doesn't access the field d)
         int i;
-        heapify(Q, 0);
         for(i = 0; i < Q->num_of_elem; ++i) {
             if (((Node*)Q->A)[i].key == v->key)
                 ((Node*)Q->A)[i].d = u->d + w;
         }
-        heapify(Q, 0);
-        // Update distance in the graph's set of nodes V
+                // Update distance in the graph's set of nodes V
         decrease_dist(v, u->d + w);
         v->pred = u;
     }
+    printf("\tQ.A after modification: ");
+    for(int i = 0; i < Q->num_of_elem; ++i)
+        printf("[%d, %d] ", ((Node*)Q->A)[i].key, ((Node*)Q->A)[i].d);
+    printf("\n");
+    heapify(Q, 0);
+    printf("\tQ.A after heapify2: ");
+    for(int i = 0; i < Q->num_of_elem; ++i)
+        printf("[%d, %d] ", ((Node*)Q->A)[i].key, ((Node*)Q->A)[i].d);
+    printf("\n");
 }
 
 void dijkstra_binheap(Graph* G, int s)
@@ -61,31 +77,19 @@ void dijkstra_binheap(Graph* G, int s)
     for (int i = 0; i < G->N; ++i)
         memcpy(&A[i], &G->V[i], sizeof(Node));
     binheap_type* Q = build_heap(A, G->N, G->N, sizeof(Node), leq_d); // All the nodes are in Q at the beginning of the computation
+    printf("\tQ.A after heapify: ");
+    for(int i = 0; i < Q->num_of_elem; ++i)
+        printf("[%d, %d] ", ((Node*)Q->A)[i].key, ((Node*)Q->A)[i].d);
+    printf("\n");
     while (!is_heap_empty(Q)) {   // One node u is extracted at each iteration, the one with minimum d
         Node* u = extract_min(Q);
         printf("Node %d\n", u->key);
         ListNode* tmp = G->adjacencyList[u->key];
         while(tmp) {   // iterates on the adjacency list of u
-            relax_heap(Q, &G->V[u->key], &G->V[(int)((Pair*)tmp->T)->a], (int)((Pair*)tmp->T)->b);
+            relax_heap(Q, &G->V[u->key], &G->V[((Pair*)tmp->T)->a], ((Pair*)tmp->T)->b);
             printf("\tInside while. Node %d, weigth %d (tmp)\n", ((Pair*)tmp->T)->a, ((Pair*)tmp->T)->b);
-            Node* v = &G->V[((Pair*)tmp->T)->a];
-            int w = (int)((Pair*)tmp->T)->b;
-            printf("\tNode [%d, %d]. Graph node: [%d, %d]. ", v->key, v->d,
-                                                            G->V[(int)((Pair*)tmp->T)->a].key, G->V[(int)((Pair*)tmp->T)->a].d);
-            if (!is_heap_empty(Q))
-                printf("Queue min node: [%d, %d]\n", ((Node*)min_value(Q))->key, ((Node*)min_value(Q))->d);
-            else
-                printf("Queue empty\n");
-            printf("\tQ.A: ");
-            for(int i = 0; i < Q->num_of_elem; ++i)
-                printf("[%d, %d] ", ((Node*)Q->A)[i].key, ((Node*)Q->A)[i].d);
-            printf("\n");
-            print_heap(Q, printNode);
-            printf("\tG.V: ");
-            for(int i = 0; i < G->N; ++i)
-                printf("[%d, %d] ", G->V[i].key, G->V[i].d);
-            printf("\n\n");
             tmp = tmp->next;
         }
     }
+    delete_heap(Q);
 }
